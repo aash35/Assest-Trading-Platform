3f06915ae86562664ebcf73172b529dada4d2840
Chris Paterson <chris.paterson.au@gmail.com>
2021-04-27 15:47:30 +1000

    Updated Test 0: Now also constructs an asset and a user object, using dummy information.

--- src/Common/UnitTests.java
+++ src/Common/UnitTests.java
@@ -6,13 +6,26 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UnitTests {
 
     /*
-    Test 0: Construct a new organisation unit, with a name and current budget
+    Test 0: Construct a new organisation unit, asset and user objects. In complete program, these will
+    be constructed from database information.
      */
     @BeforeEach @Test
     public void newOrganisationalUnit() {
-        String name = "New Unit";
+        String OUname = "New Unit";
         int budget = 100;
-
+         // Organisational Units should be constructed with a name and budget.
         OrginisationalUnit OU = new OrginisationalUnit(name, budget);
+
+        String assetName = "New Asset";
+
+        Asset asset = new Asset(assetName);
+
+        String userID = "john.smith@wherever.com";
+        String userFirstName = "John";
+        String userLastName = "Smith";
+
+        User johnSmith = new User(userID, userFirstName, userLastName, OU);
     }
+
+
 }
