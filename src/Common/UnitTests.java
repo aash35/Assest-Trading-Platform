388bcbe63756fe26413e72573d7ccf393eb153c2
Chris Paterson <chris.paterson.au@gmail.com>
2021-04-27 15:38:35 +1000

    Implemented Test 0: Construction of new organisational unit.

--- src/Common/UnitTests.java
+++ src/Common/UnitTests.java
@@ -4,4 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class UnitTests {
+
+    /*
+    Test 0: Construct a new organisation unit, with a name and current budget
+     */
+    @BeforeEach @Test
+    public void newOrganisationalUnit() {
+        String name = "New Unit";
+        int budget = 100;
+
+        OrginisationalUnit OU = new OrginisationalUnit(name, budget);
+    }
 }
