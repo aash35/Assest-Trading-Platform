c319f8a259a95fb89dd79d82cd619cad081c95b0
Chris Paterson <chris.paterson.au@gmail.com>
2021-05-14 10:47:55 +1000

    Added a test to construct class objects before each test, to make things easier. Also changed some commenting.

--- src/test/java/UnitTests/UnitTests.java
+++ src/test/java/UnitTests/UnitTests.java
@@ -22,12 +22,43 @@ import java.util.Hashtable;
 
 public class UnitTests {
 
-    //Test 1: Construct a new Asset Type
+    /**
+     * Pre-Test class declaration
+     */
+    AssetType type;
+    OrganisationalUnit OU;
+    User user;
+    Asset asset;
+
+    /**
+     * Test 0: Construct objects for AssetType, OrganisationalUnit, User and Asset classes.
+     */
+    @BeforeEach @Test
+    public void tradeTestData() {
+        type = new AssetType();
+        type.setName("Potatoes");
+
+        OU = new OrganisationalUnit();
+        OU.setUnitName("Unit A");
+        OU.setAvailableCredit(100);
+
+        user = new User();
+        user.setUsername("User");
+        user.setHashedPassword(SHA256HashHelper.generateHashedString("password"));
+        user.setOrganisationalUnit(OU);
+        user.setAccountRoleType(AccountTypeRole.Standard);
+
+        asset = new Asset();
+        asset.setAssetType(type);
+        asset.setQuantity(50);
+        asset.setCreatedByUserID(user);
+    }
+
+    /**
+     * Test 1: Construct a new Asset Type
+     */
     @Test
     public void newAssetTypeTest() {
-        AssetType type = new AssetType();
-        type.setName("123");
-
         Client client = new Client();
 
         JsonPayloadRequest request = new JsonPayloadRequest();
@@ -41,13 +72,11 @@ public class UnitTests {
         assertNull(response.getPayloadObject());
     }
 
-    //Test 2: Construct a new Organisational Unit
+    /**
+     * Test 2: Construct a new Organisational Unit
+     */
     @Test
     public void newOrganisationalUnit() {
-        OrganisationalUnit OU = new OrganisationalUnit();
-        OU.setUnitName("Unit A");
-        OU.setAvailableCredit(100);
-
         Client client = new Client();
 
         JsonPayloadRequest request = new JsonPayloadRequest();
@@ -60,16 +89,11 @@ public class UnitTests {
     }
 
 
-    //Test 3: Construct a new User
+    /**
+     * Test 3: Construct a new User
+     */
     @Test
     public void newUser() {
-        User user = new User();
-        user.setUsername("User");
-        user.setHashedPassword(SHA256HashHelper.generateHashedString("password"));
-        OrganisationalUnit OU = new OrganisationalUnit(); OU.setUnitName("Unit");
-        user.setOrganisationalUnit(OU);
-        user.setAccountRoleType(AccountTypeRole.Standard);
-
         Client client = new Client();
 
         JsonPayloadRequest request = new JsonPayloadRequest();
@@ -79,20 +103,5 @@ public class UnitTests {
 
         JsonPayloadResponse response = client.SendRequest(request);
         assertNull(response.getPayloadObject());
-    }
-
-
-    //Test 3: Construct an illegal buy type Trade and ensure it throws an exception.
-
-    @Test
-    public void newIllegalBuy() {
-
-    }
-
-    //Test 4: Construct an illegal sell type Trade and ensure it throws an exception.
-
-    @Test
-    public void newIllegalSell() {
-
     }
 }
