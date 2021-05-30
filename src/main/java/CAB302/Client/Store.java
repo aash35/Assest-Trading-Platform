9312881726671fbe65c4a29a5df52ea5d713c1ea
Chris Paterson <chris.paterson.au@gmail.com>
2021-05-30 11:15:08 +1000

    Began construction of Store GUI page. Added createPanel, getAssetsList and layoutAssetsPanel methods.

--- src/main/java/CAB302/Client/Store.java
+++ src/main/java/CAB302/Client/Store.java
@@ -1,28 +1,53 @@
 package CAB302.Client;
 
+import CAB302.Common.AssetType;
+import CAB302.Common.BaseObject;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
 
 public class Store extends JPanel {
-    GridBagConstraints gbc = new GridBagConstraints();
-
-    JLabel storeLabel = new JLabel("Store");
+    private JPanel titlePanel;
+    private JPanel assetsPanel;
+    private List<BaseObject> assetsList;
 
     public Store(JFrame frame) {
-        setBackground(new Color(243, 244, 246));
-        setLayout(new GridBagLayout());
-        //gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.weightx = 0.5;
-        gbc.weighty = 0.5;
+        Color c = new Color(243, 244, 246);
+        setLayout(new BorderLayout());
+
+        titlePanel = createPanel(c);
+        assetsPanel = createPanel(c);
+
+        add(titlePanel, BorderLayout.NORTH);
+        add(assetsPanel, BorderLayout.CENTER);
+
+        JLabel storeLabel = new JLabel("Store");
+        storeLabel.setFont(new Font(storeLabel.getFont().getFontName(), Font.PLAIN, 42));
+        titlePanel.add(storeLabel);
+    }
+
+    private JPanel createPanel(Color c){
+        JPanel panel = new JPanel();
+        panel.setBackground(c);
+        return panel;
+    }
+
+    private void getAssetsList() {
+        AssetType emptyType = new AssetType();
+
+        assetsList = emptyType.list();
+    }
+
+    private void layoutAssetsPanel() {
+        assetsPanel.setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        constraints.weightx = 0.5;
+        constraints.weighty = 0.5;
 
         // First Column
-        gbc.anchor = GridBagConstraints.CENTER;
-        gbc.insets = new Insets(0, 0, 0, 0);
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-
-        storeLabel.setFont(new Font(storeLabel.getFont().getFontName(), Font.PLAIN, 42));
-
-        add(storeLabel, gbc);
+        constraints.anchor = GridBagConstraints.FIRST_LINE_START;
+        constraints.insets = new Insets(0, 0, 0, 0);
     }
 }
