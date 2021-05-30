78a233bbb13efc5be5d33d077260a3a7e76b8d13
Chris Paterson <chris.paterson.au@gmail.com>
2021-05-30 12:20:28 +1000

    Implemented quantity and price for orders as spinner box widgets.

--- src/main/java/CAB302/Client/BuySellAsset.java
+++ src/main/java/CAB302/Client/BuySellAsset.java
@@ -10,6 +10,10 @@ public class BuySellAsset extends JPanel {
 
     private JPanel titlePanel;
     private JPanel buySellPanel;
+    private JSpinner buyQuantity;
+    private JSpinner buyPrice;
+    private JSpinner sellQuantity;
+    private JSpinner sellPrice;
 
     public BuySellAsset(JFrame frame, AssetType assetType){
         Color c = new Color(243, 244, 246);
@@ -24,6 +28,13 @@ public class BuySellAsset extends JPanel {
         JLabel assetName = new JLabel(assetType.getName());
         assetName.setFont(new Font(assetName.getFont().getFontName(), Font.PLAIN, 42));
 
+        buyQuantity = createSpinner();
+        buyPrice = createSpinner();
+        sellQuantity = createSpinner();
+        sellPrice = createSpinner();
+
+
+
         //gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.weightx = 0.5;
         gbc.weighty = 0.5;
@@ -33,12 +44,17 @@ public class BuySellAsset extends JPanel {
         gbc.insets = new Insets(0, 0, 0, 0);
         gbc.gridx = 0;
         gbc.gridy = 0;
-
     }
 
     private JPanel createPanel(Color c){
         JPanel panel = new JPanel();
         panel.setBackground(c);
         return panel;
+    }
+
+    private JSpinner createSpinner(){
+        SpinnerModel model = new SpinnerNumberModel(1, 1, null, 1);
+        JSpinner spinner = new JSpinner(model);
+        return  spinner;
     }
 }
