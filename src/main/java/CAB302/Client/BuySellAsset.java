2c08d109c9a4eea1644c66824b4afd8a106d819a
Chris Paterson <chris.paterson.au@gmail.com>
2021-05-30 10:38:30 +1000

    Implemented basic constructor for buySellAsset class. Will be updated later.

--- src/main/java/CAB302/Client/BuySellAsset.java
+++ src/main/java/CAB302/Client/BuySellAsset.java
@@ -1,7 +1,31 @@
 package CAB302.Client;
 
+import CAB302.Common.AssetType;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class BuySellAsset extends JPanel {
+    GridBagConstraints gbc = new GridBagConstraints();
+
+    JLabel buySellLabel = new JLabel("Buy or Sell Asset");
+
+    public BuySellAsset(JFrame frame, AssetType assetType){
+        setBackground(new Color(243, 244, 246));
+        setLayout(new GridBagLayout());
+
+        //gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weightx = 0.5;
+        gbc.weighty = 0.5;
+
+        // First Column
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(0, 0, 0, 0);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+        buySellLabel.setFont(new Font(buySellLabel.getFont().getFontName(), Font.PLAIN, 42));
+
+        add(buySellLabel, gbc);
+    }
 }
