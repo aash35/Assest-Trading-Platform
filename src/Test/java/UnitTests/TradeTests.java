c86ef04394636fe8c148fef0eab9f64bc68c7791
Chris Paterson <chris.paterson.au@gmail.com>
2021-06-06 15:11:36 +1000

    Added the createTrade unit test.

--- src/Test/java/UnitTests/TradeTests.java
+++ src/Test/java/UnitTests/TradeTests.java
@@ -1,26 +1,24 @@
 package UnitTests;
 
 import CAB302.Client.Client;
-import CAB302.Common.Asset;
-import CAB302.Common.AssetType;
+import CAB302.Common.*;
 import CAB302.Common.Enums.AccountTypeRole;
 import CAB302.Common.Enums.RequestPayloadType;
+import CAB302.Common.Enums.TradeStatus;
+import CAB302.Common.Enums.TradeTransactionType;
 import CAB302.Common.Helpers.SHA256HashHelper;
-import CAB302.Common.OrganisationalUnit;
 import CAB302.Common.ServerPackages.PayloadRequest;
 import CAB302.Common.ServerPackages.PayloadResponse;
-import CAB302.Common.User;
 import CAB302.Server.Server;
+import org.junit.Assert;
 import org.junit.Before;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TradeTests {
     /**
      * Pre-Test class declaration
@@ -35,10 +33,9 @@ public class TradeTests {
     public void preTestConstruction() {
         client = new Client();
 
-        AssetTests assetTests = new AssetTests();
-        assetTests.createAsset();
-        asset = assetTests.asset;
-        OU = assetTests.OU;
+        OrganisationalUnitTests ouTests = new OrganisationalUnitTests();
+        ouTests.createOrganisationalUnit();
+        OU = ouTests.OU;
 
         AssetTypeTests assetTypeTests = new AssetTypeTests();
         assetTypeTests.createAssetType();
@@ -67,8 +64,33 @@ public class TradeTests {
      * Test 0: Construct objects for AssetType, OrganisationalUnit, User and Asset classes.
      */
     @Test
+    @Order(1)
     public void createTrade() {
+        Client client = new Client();
 
+        Trade trade = new Trade();
+        trade.setAssetType(type);
+        trade.setOrganisationalUnit(OU);
+        trade.setQuantity(10);
+        trade.setPrice(10);
+        trade.setTransactionType(TradeTransactionType.Buying);
+        trade.setStatus(TradeStatus.InMarket);
+
+        PayloadRequest request = new PayloadRequest();
+        request.setPayloadObject(trade);
+        request.setRequestPayloadType(RequestPayloadType.Buy);
+
+        PayloadResponse response = null;
+
+        try {
+            response = client.SendRequest(request);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertNotNull(response);
+
+        Assert.assertNotNull(response.getPayloadObject());
     }
 
     @Test
